14c4f0fc8afdb049dd275e567dd5980c44454ab6
Devin Lane <silvertenor@icloud.com>
2023-02-01 18:08:13 -0500

    added option to include local scripts

--- src/fabproj_manager/fabproj.py
+++ src/fabproj_manager/fabproj.py
@@ -10,10 +10,11 @@ cwd = os.getcwd()
 @click.command()
 # @click.argument("command")
 @click.option('--command', '-c', default='new', help='[new] creates a new project')
-@click.option("--name", "-n", default='myProj', help='Name of your fabric project')
+@click.option("--name", "-n", default='myProj', show_default=True, help='Name of your fabric project')
+@click.option('--local', '-l', default=False, show_default=True, help='Whether project will need local scripts or not')
 
 
-def create_project(command, name):
+def create_project(command, name, local):
     '''
     This tool will help you create new fabric project templates, including
     directory structure.
@@ -29,6 +30,9 @@ def create_project(command, name):
             os.makedirs(os.path.join(rootdir, "download"))
             os.makedirs(os.path.join(rootdir, "include/img"))
             os.makedirs(os.path.join(rootdir, "logs"))
+            if local is True:
+                os.makedirs(os.path.join(rootdir, 'local'))
+            
             shutil.copy(os.path.join(dirname,'modules/img/fabric_logo.png'), os.path.join(rootdir, 'include/img/fabric_logo.png'))
             ipyCreate.create_fab_notebook(name, rootdir)
 
