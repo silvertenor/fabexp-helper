6dccd9c4a737d8fc8261b7f1033f45ae0051c7a7
Devin Lane <silvertenor@icloud.com>
2023-01-31 22:46:47 -0500

    initial commit

---
+++ src/fabproj_manager/fabproj.py
@@ -0,0 +1,27 @@
+# cli.py
+import click, os, sys
+dirname = os.path.dirname(__file__)
+sys.path.append(dirname)
+import modules.ipyCreate as ipyCreate
+
+dirname = os.getcwd()
+
+
+@click.command()
+@click.argument("command")
+@click.option("--name", "-n")
+
+
+def create_project(command, name):
+    if command == "add":
+        confirmation = input(
+            f"Do you want to add a new project titled {name} in the current working directory? (yes/no) "
+        )
+        if confirmation == "yes":
+            os.makedirs(os.path.join(dirname, "upload"), exist_ok=True)
+            os.makedirs(os.path.join(dirname, "download"), exist_ok=True)
+            ipyCreate.create_fab_notebook(name)
+
+
+if __name__ == "__main__":
+    create_project()
---
+++ src/fabproj_manager/modules/ipyCreate.py
@@ -0,0 +1,40 @@
+import nbformat as nbf
+from datetime import date
+from getpass import getuser
+
+
+nb = nbf.v4.new_notebook()
+
+
+def create_fab_notebook(name):
+    text = [
+        f"""\
+<img src="./img/fabric_logo.png" width="500"/>
+
+# {name}
+```
+Created: {date.today()}
+Last Modified: 
+{getuser()}
+Clemson University
+<username>@clemson.edu
+```
+
+<div style="page-break-after: always; visibility: hidden"> 
+\pagebreak 
+</div>"""
+    ]
+
+    code = """\
+from fabrictestbed_extensions.fablib.fablib import FablibManager as fablib_manager
+
+try:
+    fablib = fablib_manager()
+                     
+    fablib.show_config()
+except Exception as e:
+    print(f"Exception: {e}")"""
+
+    nb["cells"] = [nbf.v4.new_markdown_cell(text[0]), nbf.v4.new_code_cell(code)]
+
+    nbf.write(nb, "test.ipynb")
