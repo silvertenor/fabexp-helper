776feea2ac13277bb438a2d691ba0a79afee6a4d
Devin Lane <silvertenor@icloud.com>
2023-01-31 23:04:40 -0500

    updated

--- src/fabproj_manager/fabproj.py
+++ src/fabproj_manager/fabproj.py
@@ -4,7 +4,7 @@ dirname = os.path.dirname(__file__)
 sys.path.append(dirname)
 import modules.ipyCreate as ipyCreate
 
-dirname = os.getcwd()
+cwd = os.getcwd()
 
 
 @click.command()
@@ -18,10 +18,10 @@ def create_project(command, name):
             f"Do you want to add a new project titled {name} in the current working directory? (yes/no) "
         )
         if confirmation == "yes":
-            os.makedirs(os.path.join(dirname, "upload"), exist_ok=True)
-            os.makedirs(os.path.join(dirname, "download"), exist_ok=True)
-            os.makedirs(os.path.join(dirname, "include"), exist_ok=True)
-            shutil.copy('./modules/img/fabric_logo.png', './include/img/fabric_logo.png')
+            os.makedirs(os.path.join(cwd, "upload"), exist_ok=True)
+            os.makedirs(os.path.join(cwd, "download"), exist_ok=True)
+            os.makedirs(os.path.join(cwd, "include"), exist_ok=True)
+            shutil.copy('/modules/img/fabric_logo.png', './include/img/fabric_logo.png')
             ipyCreate.create_fab_notebook(name)
 
 
